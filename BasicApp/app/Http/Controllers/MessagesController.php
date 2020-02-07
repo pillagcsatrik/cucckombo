f0d2b1e0a663ccb78cc30b5a6f690647e121b3b1
unknown <pillagcsatrik>
2020-02-07 09:00:54 +0100

    Adatok elküldése az adatbázisba

--- BasicApp/app/Http/Controllers/MessagesController.php
+++ BasicApp/app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -12,6 +13,14 @@ class MessagesController extends Controller
         'name'=>'required',
         'email'=>'required'
       ]);
-      return 'SUCCESS';
+      //return 'SUCCESS';
+      $message=new Message;
+      $message->name=$request->input('name');
+      $message->email=$request->input('email');
+      $message->message=$request->input('message');
+
+      $message->save();
+
+      return redirect('/');
     }
 }
